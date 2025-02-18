acbdd1a217a83cc309c405e172e0d3c015732f0a
Edis-mar <junioredismar18@gmail.com>
2025-02-18 12:51:14 +0000

    exercicios DOM finalizados

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-function valorPagar(){
-    var inPor15 = document.getElementById("inPor15");
-    var inTempo = document.getElementById("inTempo");
-    var outValorFinal = document.getElementById("outValorFinal");
-    var Por15 = inPor15.value;
-    var Tempo = inTempo.value;
-    var Valor = Math.ceil(Tempo / 15) * Por15;
-    outValorFinal.textContent = "Valor a pagar R$: "+Valor.toFixed(2);
+function VerPromocao(){
+    var inProduto = document.getElementById("inProduto")
+    var inPreco = document.getElementById("inPreco")
+    var outProduto = document.getElementById("outProduto")
+    var outDesconto = document.getElementById("outDesconto")
+    var Produto = inProduto.value
+    var Preco = inPreco.value
+    var promocao = (Preco * 2) + (Preco / 2)
+    var desconto = Preco/2
+    outProduto.textContent = (Produto+ " - Promoção: Leve 3 por R$: " +promocao.toFixed(2))
+    outDesconto.textContent = ("O 3° produto custa apenas R$: " +desconto.toFixed(2))
 }
-
-var btCalcule = document.getElementById("btCalcule");
-btCalcule.addEventListener("click", valorPagar);
-
+var btPromocao = document.getElementById("btPromocao")
+btPromocao.addEventListener("click", VerPromocao)
