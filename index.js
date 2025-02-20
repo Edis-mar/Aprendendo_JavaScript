54a6c1d03061c445de73ca687b946a8cc3002fb4
Edis-mar <junioredismar18@gmail.com>
2025-02-20 14:34:00 +0000

    exercicios de condição

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-function VerPromocao(){
-    var inProduto = document.getElementById("inProduto")
-    var inPreco = document.getElementById("inPreco")
-    var outProduto = document.getElementById("outProduto")
-    var outDesconto = document.getElementById("outDesconto")
-    var Produto = inProduto.value
-    var Preco = inPreco.value
-    var promocao = (Preco * 2) + (Preco / 2)
-    var desconto = Preco/2
-    outProduto.textContent = (Produto+ " - Promoção: Leve 3 por R$: " +promocao.toFixed(2))
-    outDesconto.textContent = ("O 3° produto custa apenas R$: " +desconto.toFixed(2))
+function PesoIdeal(){
+    var inNome = document.getElementById("inNome")
+    var inAltura = document.getElementById("inAltura")
+    var rbMasculino = document.getElementById("rbMasculino")
+    var rbFeminino = document.getElementById("rbFeminino")
+    outResultado = document.getElementById("outResultado")
+    var nome = inNome.value
+    var altura = Number(inAltura.value)
+    var masculino = rbMasculino.checked
+    var feminino = rbFeminino.checked
+    if (nome == "" || (masculino == false && feminino == false)){
+        alert("Por favor, informe o nome e selecione o sexo...")
+        inNome.focus()
+        return
+    }
+    if (altura == 0 || isNaN(altura)){
+        alert("Por favor , informe a altura corretamente...")
+        inAltura.focus()
+        return
+    }
+    if (masculino) {
+        var peso = 22 * Math.pow(altura,2)
+    }
+    else{
+        var peso = 21 * Math.pow(altura, 2)
+    }
+    
+    outResultado.textContent = nome + ": seu peso ideal é " +peso.toFixed(3) +" kg"
 }
-var btPromocao = document.getElementById("btPromocao")
-btPromocao.addEventListener("click", VerPromocao)
+var btResultado = document.getElementById("btPeso")
+btResultado.addEventListener("click", PesoIdeal)
+
+function LimparCampos(){
+
+}
