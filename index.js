b7bee4e5b0a9a056c0f2d41c5641be03b6492d5b
Edis-mar <junioredismar18@gmail.com>
2025-02-24 13:29:07 +0000

    condição para casos de horarios

--- index.js
+++ index.js
@@ -1,35 +1,17 @@
-function PesoIdeal(){
-    var inNome = document.getElementById("inNome")
-    var inAltura = document.getElementById("inAltura")
-    var rbMasculino = document.getElementById("rbMasculino")
-    var rbFeminino = document.getElementById("rbFeminino")
-    outResultado = document.getElementById("outResultado")
-    var nome = inNome.value
-    var altura = Number(inAltura.value)
-    var masculino = rbMasculino.checked
-    var feminino = rbFeminino.checked
-    if (nome == "" || (masculino == false && feminino == false)){
-        alert("Por favor, informe o nome e selecione o sexo...")
-        inNome.focus()
+function CalcularFuso(){
+    var inHoraBrasil = document.getElementById("inHoraBrasil")
+    var outHoraFranca = document.getElementById("outHoraFranca")
+    var horaBrasil = Number(inHoraBrasil.value)
+    if (inHoraBrasil.value == "" || isNaN(horaBrasil)){
+        alert("Informe a hora no Brasil corretamente")
+        inHoraBrasil.focus()
         return
-    }
-    if (altura == 0 || isNaN(altura)){
-        alert("Por favor , informe a altura corretamente...")
-        inAltura.focus()
-        return
-    }
-    if (masculino) {
-        var peso = 22 * Math.pow(altura,2)
-    }
-    else{
-        var peso = 21 * Math.pow(altura, 2)
-    }
-    
-    outResultado.textContent = nome + ": seu peso ideal é " +peso.toFixed(3) +" kg"
+    } 
+    var fuso = horaBrasil + 5 
+    if (fuso > 24) {
+        fuso = fuso - 24
+    } 
+    outHoraFranca.textContent = "Hora na França: "+fuso.toFixed(2)
 }
-var btResultado = document.getElementById("btPeso")
-btResultado.addEventListener("click", PesoIdeal)
-
-function LimparCampos(){
-
-}
+btExibir = document.getElementById("btExibir")
+btExibir.addEventListener("click" , CalcularFuso)
