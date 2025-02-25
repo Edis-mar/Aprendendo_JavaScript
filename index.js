a5af1a2595a8a4ee7f0de4b027928ea6f198b800
Edis-mar <junioredismar18@gmail.com>
2025-02-25 12:33:38 +0000

    exercicio de velocidade

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
-function RetirarSaque(){
-    var inSaque = document.getElementById("inSaque")
-    var out100 = document.getElementById("out100")
-    var out50 = document.getElementById("out50")
-    var out10 = document.getElementById("out10")
-    var saque = Number(inSaque.value)
-    if(saque == "" || isNaN(saque)){
-        alert("informe o valor do saque corretamente.")
-        inSaque.focus()
-        return
+function VerificarNumero(){
+    var inVelo = document.getElementById("inVelo")
+    var inCondu = document.getElementById("inCondu")
+    var outSituacao = document.getElementById("outSituacao")
+    var velo = Number(inVelo.value)
+    var condu = Number(inCondu.value)
+    if(condu <= velo){
+        outSituacao.textContent = "Situação: sem Multa"
     }
-    if (saque % 10 != 0) {
-        alert("valor inválido para notas disponiveis (R$ 10, 50, 100)")
-        inSaque.focus()
-        return
+    else if(condu <= velo * 1.2){
+        outSituacao.textContent = "Situação: Multa leve "
     }
-    var notas100 = Math.floor(saque / 100)
-    var resto = saque % 100
-    var notas50 = Math.floor(resto / 50)
-    resto = resto % 50
-    var notas10 = Math.floor(resto / 10)
-    out100.textContent = ("Notas de R$ 100: " +notas10)
-    out50.textContent = ("Notas de R$ 50: "+notas50)
-    out10.textContent = ("Notas de R$ 10: "+notas10)
+    else{
+        outSituacao.textContent = "Situação: Multa Grave "
+    }
 }
-
 btExibir = document.getElementById("btExibir")
-btExibir.addEventListener("click", RetirarSaque)
+btExibir.addEventListener("click", VerificarNumero)
