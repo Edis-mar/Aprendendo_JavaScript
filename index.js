30e1c0ab389b6b5a0ddee6420818fd43de6041ff
Edis-mar <junioredismar18@gmail.com>
2025-02-25 12:00:43 +0000

    condicional - caixa eletrônico

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-function CalcularFuso(){
-    var inHoraBrasil = document.getElementById("inHoraBrasil")
-    var outHoraFranca = document.getElementById("outHoraFranca")
-    var horaBrasil = Number(inHoraBrasil.value)
-    if (inHoraBrasil.value == "" || isNaN(horaBrasil)){
-        alert("Informe a hora no Brasil corretamente")
-        inHoraBrasil.focus()
+function RetirarSaque(){
+    var inSaque = document.getElementById("inSaque")
+    var out100 = document.getElementById("out100")
+    var out50 = document.getElementById("out50")
+    var out10 = document.getElementById("out10")
+    var saque = Number(inSaque.value)
+    if(saque == "" || isNaN(saque)){
+        alert("informe o valor do saque corretamente.")
+        inSaque.focus()
         return
-    } 
-    var fuso = horaBrasil + 5 
-    if (fuso > 24) {
-        fuso = fuso - 24
-    } 
-    outHoraFranca.textContent = "Hora na França: "+fuso.toFixed(2)
+    }
+    if (saque % 10 != 0) {
+        alert("valor inválido para notas disponiveis (R$ 10, 50, 100)")
+        inSaque.focus()
+        return
+    }
+    var notas100 = Math.floor(saque / 100)
+    var resto = saque % 100
+    var notas50 = Math.floor(resto / 50)
+    resto = resto % 50
+    var notas10 = Math.floor(resto / 10)
+    out100.textContent = ("Notas de R$ 100: " +notas10)
+    out50.textContent = ("Notas de R$ 50: "+notas50)
+    out10.textContent = ("Notas de R$ 10: "+notas10)
 }
+
 btExibir = document.getElementById("btExibir")
-btExibir.addEventListener("click" , CalcularFuso)
+btExibir.addEventListener("click", RetirarSaque)
