9d11d37fa893c48eba1d746cd45b8ab89237e92d
Edis-mar <junioredismar18@gmail.com>
2025-02-27 13:31:10 +0000

    laço for, tabuada e decrescente

--- index.js
+++ index.js
@@ -1,31 +1,19 @@
-function VerificarLado(){
-    var inA = document.getElementById("inA")
-    var inB = document.getElementById("inB")
-    var inC = document.getElementById("inC")
-    var outLados = document.getElementById("outLados")
-    var outTipo = document.getElementById("outTipo")
-    var A = Number(inA.value)
-    var B = Number(inB.value)
-    var C = Number(inC.value)
-if (isNaN (A) || isNaN (B) || isNaN (C) || A <= 0 || B <= 0 || C <= 0){
-    alert ("Escreva os numeros corretamente")
+function MostrarTabuada (){
+    var inNumero = document.getElementById("inNumero")
+    var outTabuada = document.getElementById("outTabuada")
+    var num = Number(inNumero.value)
+    if (num == 0 || isNaN(num)) {
+        alert ("Informe um numero válido...")
+        inNumero.focus();
+        return;
+    } 
+    var resposta =" Entre " + num + " e 1: "
+
+    for (let i = num ; i > 0; i--) {
+        resposta = resposta + i + ","
+    } 
+
+    outTabuada.textContent = resposta
 }
-else if(A > (B + C) || B > (A + C) || C > (B + A)){
-    outLados.textContent = "Lados não podem formar um triângulo"
-    outTipo.textContent = ""
-}
-else if(A == B && B == C && C == A ){
-    outLados.textContent = "Lados podem formar um triângulo "
-    outTipo.textContent = "Tipo: Equilátero"
-}
-else if(A == B && A != C || B == C && B != A || C == A && C != B){
-    outLados.textContent = "Lados podem formar um triângulo "
-  outTipo.textContent = "Tipo: Isósceles"
-}
-else if(A != B && A != C && C != B ){
-    outLados.textContent = "Lados podem formar um triângulo "
-    outTipo.textContent = "Tipo: Escaleno"
-}
-}
-btVerificar = document.getElementById("btVerificar")
-btVerificar.addEventListener("click", VerificarLado)
+var btMostrar = document.getElementById("btMostrar")
+btMostrar.addEventListener("click", MostrarTabuada)
