de9a0bf54ab4381f10e91542d6e543c24be9c22f
Edis-mar <junioredismar18@gmail.com>
2025-03-05 16:27:49 +0000

    esse aqui fritou um poukito meu cerebro

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
-var simbol =("*_")
-function Simbolos(){
+function RepetirFruta(){
     var inNumero = document.getElementById("inNumero")
-    var outSimbolos = document.getElementById("outSimbolos")
+    var inFruta = document.getElementById("inFruta")
+    var outFr = document.getElementById("outFr")
+    var fruta=inFruta.value
     var num = Number(inNumero.value)
-    if(num == 0 || isNaN(num)){
+    if(fruta == ""){
+        alert("Escreva uma fruta")
+        inFruta.focus()
+        return
+    }else if(/\d/.test(fruta)){
+        alert("Escreva uma fruta")
+        inFruta.focus()
+        return 
+    }else if(num == 0 || isNaN(num)){
         alert("Número inválido")
         inNumero.focus()
         return
     }
-    var estrelas = ("")
-    for(i=1; i <= num; i++){
-        estrelas = estrelas + "*_"
-        outSimbolos.textContent = (estrelas + "*")
-    }
+    var simbolo =" * "
+    var repeticao = ""
+    for(i=1; i<=num; i++){
+        repeticao += fruta + simbolo
+    } 
+    outFr.textContent= repeticao
 }
-
-var btPreencher = document.getElementById("btPreencher")
-btPreencher.addEventListener("click", Simbolos)
+var btRepetir = document.getElementById("btRepetir")
+btRepetir.addEventListener("click", RepetirFruta)
