b26fde5060d3406803edf6e89056c2d621a37826
Edis-mar <junioredismar18@gmail.com>
2025-03-14 19:21:08 +0000

    Vetor do Numero sorteado

--- index.js
+++ index.js
@@ -1,65 +1,68 @@
-var pacientes = []
-function adicionarPaciente(){
-    var inPaciente = document.getElementById("inPaciente")
-    var outLista = document.getElementById("outLista")
-    var nome = inPaciente.value
-    if (nome == ""){
-        alert("Informe o nome do Paciente:")
-        inPaciente.focus()
+var erros = [] //VETOR ACUMULADOR DOS ERROS
+var sorteado = Math.floor(Math.random() *100) +1 //NUMERO SORTEADO
+const chances = 6 //NUMERO DE CHANCES
+
+function apostarNumero(){
+
+    //DECLARAR VARIAVEIS
+    var inNumero = document.getElementById("inNumero")
+    var num = Number(inNumero.value)
+
+    // VERIFICAR O VALOR NO INPUT
+    if (isNaN(num) || num <= 0 || num > 100){
+        alert("Escreva um numero válido")
+        inNumero.focus()
         return
     }
-    pacientes.push(nome)
-    var lista = ""
-    for(i = 0; i < pacientes.length; i++){
-        lista += (i + 1) + ". " + pacientes[i] + "\n"
-    }
-    outLista.textContent = lista
-    inPaciente.value = ""
-    inPaciente.focus()
+
+    //DECLARAR VARIAVEIS 2
+    var outErros = document.getElementById("outErros")
+    var outChances = document.getElementById("outChances")
+    var outDica = document.getElementById("outDica")
+    
+    //SE ACERTAR
+    if (num == sorteado){
+        alert("Parabéns!!! Você Acertou")
+        btApostar.disabled = true
+        btJogar.className = "exibe"
+        outDica.textContent = "Parabéns!! Número sorteado: "+sorteado
+    }else {
+         // SE REPETIR O NUMERO ERRADO 
+        if (erros.indexOf(num) >= 0){
+            alert("você ja apostou o número "+num+ ". Tente outro...")
+
+        //SE ERRAR  
+        }else {
+            erros.push(num)
+            var numErros = erros.length
+            var numChances = chances - numErros
+            outErros.textContent = numErros + "(" + erros.join(", ")+ ")"
+            outChances.textContent = numChances
+
+            //SE AS CHANCES ACABAREM
+            if(numChances == 0){
+                alert("Suas chances acabaram...")
+                btApostar.disabled = true
+                btJogar.className = "exibe"
+                outDica.textContent = "Game Over!! Número Sorterado: "+sorteado
+
+            //ENQUANTO TEM CHANCES    
+            }else { 
+                var dica = num < sorteado ? "maior" : "menor"
+                outDica.textContent = "Dica: Tente um número " + dica + " que " +num
+                }
+            } 
+        }
+        
+        //RESETAR O INPUT
+       inNumero.value = ""
+       inNumero.focus()
 }
-var btAdd = document.getElementById("btAdd")
-btAdd.addEventListener("click", adicionarPaciente)
+var btApostar = document.getElementById("btApostar")
+btApostar.addEventListener("click", apostarNumero)
 
-
-
-function Urgencia(){
-    var inPaciente = document.getElementById("inPaciente")
-    var outLista = document.getElementById("outLista")
-    var nome = inPaciente.value
-    if (nome == ""){
-        alert("Informe o nome do Paciente:")
-        inPaciente.focus()
-        return
-    }
-    pacientes.unshift(nome)
-    var lista = ""
-    for(i = 0; i < pacientes.length; i++){
-        lista += (i + 1) + ". " + pacientes[i] + "\n"
-    }
-    outLista.textContent = lista
-    inPaciente.value = ""
-    inPaciente.focus()
+function JogarNovamente(){
+    location.reload()
 }
-var btUrg = document.getElementById("btUrg")
-btUrg.addEventListener("click", Urgencia)
-
-
-
-function atenderPaciente(){
-    if (pacientes.lenght == 0) {
-        alert("Não há pacientes na lista de espera")
-        inPaciente.focus()
-        return
-    }
-    var outAtendimento = document.getElementById("outAtendimento")
-    var outLista = document.getElementById("outLista")
-    var atender = pacientes.shift()
-    outAtendimento.textContent = atender
-    var lista = ""
-    for(i = 0; i < pacientes.length; i++){
-        lista += (i + 1) + ". " + pacientes[i] + "\n"
-    }
-    outLista.textContent = lista
-}
-var btAtt = document.getElementById("btAtt")
-btAtt.addEventListener("click", atenderPaciente)
+var btJogar = document.getElementById("btJogar")
+btJogar.addEventListener("click", JogarNovamente)
