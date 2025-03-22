dbb4cd66b25e5e84c431e09f1635d728d69c9074
Edis-mar <junioredismar18@gmail.com>
2025-03-22 13:54:14 +0000

    esse foi meio complexo kkk, tenho q aprender o nome do tipo de array usado na ultima função

--- index.js
+++ index.js
@@ -1,61 +1,69 @@
-var carros = []
+var criancas = []
+function adicionarCriança(){
+    var inNome = document.getElementById("inNome")
+    var inIdade = document.getElementById("inIdade")
+    var nome = inNome.value
+    var idade = Number(inIdade.value)
 
-function adicionarCarros (){
-    var inModelo = document.getElementById("inModelo")
-    var inPreco = document.getElementById("inPreco")
-    var modelo = inModelo.value
-    var preco = Number(inPreco.value)
-
-    if (modelo == "" || preco == 0 || isNaN(preco)){
-        alert("Informe corretamente os dados...")
-        inModelo.focus
+    if (nome == "" || idade == 0 || isNaN(idade)){
+        alert("Insira os dados corretamente")
+        inNome.focus()
         return
     }
-    carros.push( { modelo: modelo, preco:preco } )
-    inModelo.value = ""
-    inPreco.value = ""
-    inModelo.focus()
 
-    listarCarros()
+    criancas.push({nome: nome , idade:idade})
+    inNome.value = ""
+    inIdade.value = ""
+    inNome.focus()
 }
 var btAdd = document.getElementById("btAdd")
-btAdd.addEventListener("click", adicionarCarros)
+btAdd.addEventListener("click", adicionarCriança)
 
+function criarLista (){
 
-function listarCarros(){
-    if(carros.length == 0){
-        alert("Não há carros na lista")
-        return
+    if (criancas.length == 0){
+       alert("isncreva crianças na lista...")
+       return 
     }
+
     var lista = ""
 
-    for(let i in carros){
-        lista +=carros[i].modelo + " - R$: " + carros[i].preco.toFixed(2) + "\n"
+    for(var i in criancas){
+        lista += criancas[i].nome + " - " + criancas[i].idade + " anos\n"
     }
-    document.getElementById("outLista").textContent = lista
+    document.getElementById("outLista").textContent = lista 
 }
-var btListar = document.getElementById("btListar")
-btListar.addEventListener("click", listarCarros)
+btListar = document.getElementById("btListar")
+btListar.addEventListener("click", criarLista)
 
+function filtrarLista (){
+    if (criancas.length == 0){
+        alert("isncreva crianças na lista...")
+        return 
+     }
+    var copia = criancas.slice()
+    copia.sort(function (a , b) {return a.idade - b.idade} )
+    var resumo = ""
 
-function filtrarCarros(){
-    var maximo = Number(prompt("Qual o valor máximo que o cliente deseja pagar?"))
-    if (maximo == 0 || isNaN(maximo)){
-        return
+    var aux = copia[0].idade
+    var nomes = []
+
+    for(var i in copia){
+        if(copia[i].idade == aux){ // PREENCHER A LISTA NOME
+            nomes.push(copia[i].nome)
+        }else{  //CRIAR A PRIMEIRA LINHA DE BLOCOS DE CRIANÇAS
+            resumo += aux + " ano(s): " + nomes.length + " crianca(s) - "
+            resumo += (nomes.length / copia.length * 100).toFixed(2) + "%\n"
+            resumo += "(" + nomes.join(", ") + ")\n\n"
+            aux = copia[i].idade
+            nomes = []
+            nomes.push(copia[i].nome)
+        }
     }
-    var lista = ""
-    for(var i in carros){
-        if( carros[i].preco <= maximo){
-            lista += carros[i].modelo + " - R$: " + carros[i].preco.toFixed(2) + "\n"
-        }    
-    }
-    var outlista = document.getElementById("outLista")
-    if (lista == ""){
-        outlista.textContent = "Não há carros com preço até R$: "+ maximo.tofixed(2)
-    }else {
-        outlista.textContent = "Carros até R$: "+ maximo.toFixed(2) +
-         "\n----------------------------\n"+ lista
-    }
+    resumo += aux + " ano(s): " + nomes.length + " crianca(s) - "
+    resumo += (nomes.length / copia.length * 100).toFixed(2) + "%\n"
+    resumo += "(" + nomes.join(", ") + ")\n\n"
+    document.getElementById("outLista").textContent = resumo
 }
 var btFiltrar = document.getElementById("btFiltrar")
-btFiltrar.addEventListener("click", filtrarCarros)
+btFiltrar.addEventListener("click", filtrarLista) 
