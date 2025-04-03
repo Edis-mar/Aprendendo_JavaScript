7e9dd7fb7c7366d52cb9aedb9e46cd88694f3e87
Edis-mar <junioredismar18@gmail.com>
2025-04-03 03:31:07 +0000

    Programa numeros em ordem

--- index.js
+++ index.js
@@ -1,58 +1,46 @@
-var clubeS = []
-function adicionarClubes(){
-    var inClube = document.getElementById("inClube")
-    var outLista = document.getElementById("outLista")
-    var clube = inClube.value
+var listaNum = []
+function AdicionarNum(){
+    var inNum = document.getElementById("inNum")
+    var num = Number(inNum.value)
 
-    if (clube == ""){
-        alert("escreva o nome do clube...")
-        inClube.focus()
+    if(isNaN(num)){
+        alert("escreva um número válido")
+        inNum.focus()
+        return
+    }else if(listaNum.indexOf(num) !== -1){
+        alert("Não repita o numero")
+        inNum.focus()
+        inNum.value = ""
+        return
+    }else{
+        listaNum.push(num)
+        atualizarLista()
+    }
+    inNum.focus()
+    inNum.value = ""
+}
+function atualizarLista(){
+    document.getElementById("outLista").textContent = "Número:" + listaNum.join(",")
+} 
+
+var btAdd = document.getElementById("btAdd")
+btAdd.addEventListener("click", AdicionarNum)
+
+function VerificarOrdem(){
+    if (listaNum.length === 0){
+        alert("A lista está vazia")
+        inNum.focus()
         return
     }
-    clubeS.push(clube)
 
-    inClube.value = ""
-    inClube.focus()
+    for(var i = 0; i < listaNum.length - 1; i++){
+        if(listaNum[i] > listaNum[i + 1]){
+            document.getElementById("outInfo").textContent = "Atenção... os Números não estão em ordem crescente"
+            return
+        }
+    }
+    document.getElementById("outInfo").textContent = "Parábens, os Números estão em ordem crescente"
+
 }
-var btAdd = document.getElementById("btAdd")
-btAdd.addEventListener("click", adicionarClubes)
-
-function ListarClubes(){
-    if (clubeS == ""){
-        alert("A lista de Clubes esta vazia...")
-    }
-
-    var lista = ""
-    var num = 1
-    for (var i in clubeS){
-        lista += num + ". " + clubeS[i]+ "\n"
-        num++
-    }
-
-    document.getElementById("outLista").textContent = lista
-}
-var btListar = document.getElementById("btListar")
-btListar.addEventListener("click", ListarClubes)
-
-function MontarTabela(){
-    if (clubeS == ""){
-        alert("Adicione Clubes a lista")
-    }    
-    
-    var copia = clubeS.slice()
-    var confrontos = ""
-    if ( copia.length % 2 !== 0){
-        alert("Numero de times ímpares, não foi possivel criar a tabela")
-        return   
-    }
-
-    while (copia.length > 0) {
-      var time1 = copia.shift()     
-      var time2 = copia.pop()       
-      confrontos += `${time1} x ${time2}\n`
-    }
-
-    document.getElementById("outLista").textContent = confrontos
-}
-var btMontar = document.getElementById("btMontar")
-btMontar.addEventListener("click", MontarTabela)
+var btVerificar = document.getElementById("btVerificar")
+btVerificar.addEventListener("click", VerificarOrdem)
