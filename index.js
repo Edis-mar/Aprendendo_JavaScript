6044a403079387053949ef7f9e98d65f26afabf8
Edis-mar <junioredismar18@gmail.com>
2025-04-14 14:19:35 +0000

    pegando datas

--- index.js
+++ index.js
@@ -1,29 +1,16 @@
-function verSenha(){
-    var inSenha = document.getElementById("inSenha")
-    var outMensagem = document.getElementById("outMensagem")
-    var senha = inSenha.value
-    var erros = []
+var parcelas = Number(prompt("Quantas Parcelas? "))
+var lista = ""
+var dia, mes, ano, diaZero, mesZero
+var data = new Date()
+for(var i = 1; i <= parcelas; i++){
+    data.setMonth(data.getMonth() + 1)
+    dia = data.getDate()
+    mes = data.getMonth()+1
+    ano = data.getFullYear()
 
-    if (senha.length < 8 || senha.length > 15){
-        erros.push("possuir entre 8 e 15 caracteres")
-    }
-    if(senha.match(/[0-9]/g) == null){
-        erros.push("possuir números (no mínimo, 1)")
-    }
-    if (!senha.match(/[a-z]/g)){
-        erros.push("possuir letras minúsculas (no minimo, 1")
-    }
-    if (!senha.match(/[A-Z]/g) || senha.match(/[A-Z]/g.length == 1)){
-        erros.push("possuir letras maiusculas (no mínimo, 2")
-    }
-    if(!senha.match(/[\W|_]/g)){
-        erros.push("possuir simbolos (no minimo, 1)")
-    }
-    if(erros.length == 0){
-        outMensagem.textContent = "Ok! Senha Válida"
-    } else{
-        outMensagem.textContent = "Erro... A senha deve "+ erros.join(", ")
-    }
+    diaZero = dia < 10 ? "0" + dia : dia 
+    mesZero = mes < 10 ? "0" + mes : mes
+    
+    lista += i + "ª Parcela: " + diaZero + "/" + mesZero + "/" + ano + "\n"
 }
-var btVerificar = document.getElementById("btVerificar")
-btVerificar.addEventListener("click", verSenha)
+alert(lista)
